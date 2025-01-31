557a99eda2ea6a03a1b1e33ecf581c57c916adfd
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-01-31 20:36:15 +0000

    punto 3 completato

---
+++ scriptfattoris.js
@@ -0,0 +1,41 @@
+const animale1 = {
+    specie: "mucca",
+    razza: "chianina",
+    zampe: 4
+}
+
+function mostraanimale() {
+
+    document.getElementById("i1").innerHTML = animale1.specie;
+    document.getElementById("i2").innerHTML = animale1.razza;
+    document.getElementById("i3").innerHTML = animale1.zampe;
+
+}
+
+const animale2 = {
+    specie: "gallo",
+    razza: "Lakenfelder",
+    zampe: 2
+}
+
+function mostraanimale2() {
+
+    document.getElementById("i4").innerHTML = animale2.specie;
+    document.getElementById("i5").innerHTML = animale2.razza;
+    document.getElementById("i6").innerHTML = animale2.zampe;
+
+}
+
+const animale3 = {
+    specie: "maiale",
+    razza: "landrace",
+    zampe: 4
+}
+
+function mostraanimale3() {
+
+    document.getElementById("i7").innerHTML = animale3.specie;
+    document.getElementById("i8").innerHTML = animale3.razza;
+    document.getElementById("i9").innerHTML = animale3.zampe;
+
+}
