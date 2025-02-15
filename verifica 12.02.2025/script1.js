fc898d6dddd94693b70dcfb841b83ecdc7de3220
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-02-15 10:42:56 +0000

    esercitazioni varie

---
+++ verifica 12.02.2025/script1.js
@@ -0,0 +1,36 @@
+const CanzoneBBE = {
+    titoloDellaCanzone : "il titolo è BBE",
+    annoDiPubblicazione : "pubblicata nel 2024",
+    autore: "l'autore è Anna Pepe",
+};
+function Canzone1() {
+
+    document.getElementById("i1").innerHTML = CanzoneBBE.titoloDellaCanzone;
+    document.getElementById("i2").innerHTML = CanzoneBBE.annoDiPubblicazione;
+    document.getElementById("i3").innerHTML = CanzoneBBE.autore;
+
+}
+const CanzoneBON = {
+    titoloDellaCanzone1 : "il titolo è Bon Ton",
+    annoDiPubblicazione1 : "pubblicata nel 2023",
+    autore1: "l'autore è Anna Lazza",
+};
+function Canzone2() {
+
+    document.getElementById("i4").innerHTML = CanzoneBON.titoloDellaCanzone1;
+    document.getElementById("i5").innerHTML = CanzoneBON.annoDiPubblicazione1;
+    document.getElementById("i6").innerHTML = CanzoneBON.autore1;
+
+}
+const CanzoneScuola = {
+    titoloDellaCanzone2 : "il titolo è scuola",
+    annoDiPubblicazione2 : "pubblicata nel 2021",
+    autore2: "l'autore è Tha supreme",
+};
+function Canzone3() {
+
+    document.getElementById("i7").innerHTML = CanzoneScuola.titoloDellaCanzone2;
+    document.getElementById("i8").innerHTML = CanzoneScuola.annoDiPubblicazione2;
+    document.getElementById("i9").innerHTML = CanzoneScuola.autore2;
+
+}
