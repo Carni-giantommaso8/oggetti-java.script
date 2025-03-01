62a1231f1484f78d506f0570a5abc8cffe54657a
Carni-giantommaso8 Carni-giantommaso8 <carni.giantommaso@iisgalvanimi.edu.it>
2025-03-01 18:27:13 +0000

    primo esempio di gioco lavoro 02.4

---
+++ esercitazione minigioco avanzato/script.js
@@ -0,0 +1,103 @@
+function startGame() {
+    myGameArea.start();
+    animatedObject.loadImages();
+};
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+      this.canvas.width = 480; 
+      this.canvas.height = 270;
+      this.context = this.canvas.getContext("2d");
+      document.body.insertBefore(this.canvas, document.body.childNodes[0])
+      this.interval = setInterval(updateGameArea, 20);
+    },
+
+    draw: function(component) {
+        this.context.fillStyle = component.color;
+        this.context.fillRect(component.x, component.y, component.width, component.height);
+    },
+    drawGameObject: function(gameObject) {
+        this.context.drawImage(
+          gameObject.image,
+          gameObject.x,
+          gameObject.y,
+          gameObject.width,
+          gameObject.height
+        );  
+    },
+};
+
+function moveUp() {
+    animatedObject.speedY = -4;
+}
+
+function moveDown() {
+    animatedObject.speedY = 4;
+}
+
+function moveLeft() {
+    animatedObject.speedX = -4;
+}
+
+function moveRight() {
+    animatedObject.speedX = 4;
+}
+
+function clearMove() {
+    animatedObject.speedX = 0;
+    animatedObject.speedY = 0;
+}
+
+function updateGameArea() {
+    myGameArea.context.clearRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
+    animatedObject.update();
+    myGameArea.drawGameObject(animatedObject);
+
+}
+
+window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowUp") moveUp();
+    if (event.key === "ArrowDown") moveDown();
+    if (event.key === "ArrowLeft") moveLeft();
+    if (event.key === "ArrowRight") moveRight();
+});
+
+window.addEventListener("keyup", function(event) {
+    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) {
+        clearMove();
+    }
+});
+var animatedObject = {
+    speedX: 0,
+    speedY: 0,
+    width: 100,
+    height: 80,
+    x: 10,
+    y: 120,
+    imageList: [], 
+    contaFrame: 0, //Tiene conto di quanti frame sono passati
+    actualFrame: 0, //Specifica quale frame disegnare
+  
+    update: function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      this.contaFrame++;
+      if (this.contaFrame == 4) {
+        this.contaFrame = 0;
+        this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+        //console.log(this.actualFrame);
+        this.image = this.imageList[this.actualFrame];
+      }
+    },
+  
+    loadImages: function() {
+       for (imgPath of running) {
+        var img = new Image(this.width, this.height);
+        img.src = imgPath;
+        this.imageList.push(img);
+        //console.log(img);
+      }
+      this.image = this.imageList[this.actualFrame];
+    }
+};  
---
+++ esercitazione minigioco avanzato/sprites.js
@@ -0,0 +1,13 @@
+var running = [
+    "https://i.ibb.co/C3WYXgJH/Run-1.png",
+    "https://i.ibb.co/JjvtSYXf/Run-2.png",
+    "https://i.ibb.co/q3dsj0Ck/Run-3.png",
+    "https://i.ibb.co/Pf6mRSM/Run-4.png",
+    "https://i.ibb.co/bTRZ0cd/Run-5.png",
+    "https://i.ibb.co/pvctj9Dm/Run-6.png",
+    "https://i.ibb.co/S40971y9/Run-7.png",
+    "https://i.ibb.co/Lhn9kF66/Run-8.png",
+    "https://i.ibb.co/QvG8mR01/Run-9.png",
+    "https://i.ibb.co/21DQq7Pp/Run-10.png",
+    "https://i.ibb.co/F46jYW4w/Run-11.png"
+];
